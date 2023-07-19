e58dcccaba911150b9962652496b136c21dbc77d
Alexander <68970161+bloodNtears@users.noreply.github.com>
2023-07-19 16:00:17 +0300

    Update main.go

--- main.go
+++ main.go
@@ -1,70 +1,56 @@
 package main
 
 type TreeNode struct {
-	Value    string
-	Children []*TreeNode
+	Value  string
+	Parent *TreeNode
 }
 
-func findNearestCommonAncestor(root *TreeNode, node1, node2 *TreeNode) *TreeNode {
-	var pathToNode1, pathToNode2 []*TreeNode
+func findNearestCommonAncestor(node1, node2 *TreeNode) *TreeNode {
+	visited := make(map[*TreeNode]bool)
 
-	findPathToNode(root, node1, &pathToNode1)
-	findPathToNode(root, node2, &pathToNode2)
-
-	var commonAncestor *TreeNode
-
-	for i := 0; i < len(pathToNode1) && i < len(pathToNode2); i++ {
-		if pathToNode1[i] != pathToNode2[i] {
-			break
+	for node1 != nil {
+		visited[node1] = true
+		node1 = node1.Parent
+	}
+	
+	for node2 != nil {
+		if visited[node2] {
+			return node2
 		}
-		commonAncestor = pathToNode1[i]
+		node2 = node2.Parent
 	}
 
-	return commonAncestor
+	return nil
 }
 
-func findPathToNode(root, target *TreeNode, path *[]*TreeNode) bool {
-	if root == nil {
-		return false
-	}
-
-	*path = append(*path, root)
-
-	if root == target {
-		return true
-	}
-
-	for _, child := range root.Children {
-		if findPathToNode(child, target, path) {
-			return true
-		}
-	}
-
-	*path = (*path)[:len(*path)-1]
-	return false
-}
 
 func main() {
-	root := &TreeNode{Value: "R"}
-	root.Children = []*TreeNode{
-		{Value: "C", Children: []*TreeNode{
-			{Value: "F", Children: []*TreeNode{
-				{Value: "A"},
-				{Value: "G"},
-			}},
-			{Value: "B"},
-		}},
-		{Value: "E"},
-		{Value: "D"},
+	nodeR := &TreeNode{
+		Value:  "R",
+		Parent: nil,
+	}
+	nodeE := &TreeNode{
+		Value:  "E",
+		Parent: nodeR,
+	}
+	nodeF := &TreeNode{
+		Value:  "F",
+		Parent: nodeE,
+	}
+	nodeA := &TreeNode{
+		Value:  "A",
+		Parent: nodeF,
+	}
+	//nodeG := &TreeNode{
+	//	Value: "G",
+	//	Parent: nodeF,
+	//}
+	nodeB := &TreeNode{
+		Value:  "B",
+		Parent: nodeE,
 	}
 
-	nodeA := root.Children[0].Children[0].Children[0]
-	nodeB := root.Children[0].Children[1]
-	// nodeG := root.Children[0].Children[0].Children[1]
-	// println(nodeA.Value, nodeB.Value, nodeG.Value)
-	// println(root.Value, root.Children[0].Children[1].Value)
-
-	commonAncestor := findNearestCommonAncestor(root, nodeA, nodeB)
+	commonAncestor := findNearestCommonAncestor(nodeA, nodeB)
 	if commonAncestor != nil {
 		println("The nearest common ancestor is node:", commonAncestor.Value)
 	} else {
